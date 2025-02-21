423bd39ac8901b36b0b7d7104f53dac9fcc9c950
edoubart <edouard.barthelemy@gmail.com>
2025-02-21 11:20:01 -0500

    Add Create Campaign.

--- programs/solana-crowdfunding/src/lib.rs
+++ programs/solana-crowdfunding/src/lib.rs
@@ -6,11 +6,28 @@ declare_id!("GAiTsjthEZq2r35wZ7nV7SsjdJbEwEDEwHx1x9ug8WjG");
 pub mod solana_crowdfunding {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create(
+        context: Context<Create>,
+        name: String,
+        description: String
+    ) -> ProgramResult {
+        let campaign = &mut context.accounts.campaign;
+
+        campaign.name = name;
+        campaign.description = description;
+        campaign.amount_donated = 0;
+        campaign.admin = *context.accounts.user.key;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer=user, space=9000, seeds=[b"CAMPAIGN_DEMO".as_ref(), user.key().as_ref()]. bump)]
+    pub campaign: Account<'info, Campaign>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
